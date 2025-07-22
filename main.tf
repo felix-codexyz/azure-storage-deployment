85427a57f6bcc208681162d3daddf610802f8a2c
Felix M <felixmomodebe@gmail.com>
2025-07-21 20:31:55 -0400

    Location Hardcoded for testing

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
 module "storage" {
   source               = "./modules/storage_account"
   storage_account_name = var.storage_account_name
-  location             = var.location
-}
-
-# ADD THESE DEBUG OUTPUTS:
-output "root_location_var" {
-  value = var.location
-}
-
-output "root_storage_name_var" {
-  value = var.storage_account_name
+  location             = "australiaeast"  # Hardcoded for testing
 }
